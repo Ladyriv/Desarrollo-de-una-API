f9cb5710f433e5d86f668c7168cbfd4bf7563346
Ladyriv <leidy.riveraf@outlook.com>
2023-02-16 23:58:09 -0500

    "Controllers & routes"

---
+++ src/controllers/workoutController.js
@@ -0,0 +1,33 @@
+const workoutService = require("../services/workoutService");
+
+const getAllWorkouts = (req, res) => {
+    const allWorkouts = workoutService.getAllWorkouts();
+    res.send("Get all workouts");
+};
+
+const getOneWorkout = (req, res) => {
+    const workout = workoutService.getOneWorkout(req.params.workoutId);
+    res.send(`Get workout ${req.params.workoutId}`);
+};
+
+const createOneWorkout = (req, res) => {
+    const createdWorkout = workoutService.createWorkout(req.params.workoutId);
+    res.send(`Create workout ${req.params.workoutId}`);
+};
+
+const updateOneWorkout = (req, res) => {
+    const updatedWorkout = workoutService.updateOneWorkout(req.params.workoutId); 
+    res.send(`Update workout ${req.params.workoutId}`);
+};
+
+const deleteOneWorkout = (req, res) => {
+    workoutService.deleteOneWorkout(req.params.workoutId);
+    res.send(`Delete workout ${req.params.workoutId}`);
+};
+module.exports = {
+    getAllWorkouts,
+    getOneWorkout,
+    createOneWorkout,
+    updateOneWorkout,
+    deleteOneWorkout, 
+};
